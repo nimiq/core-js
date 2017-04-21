b116406fcd6d908d30333859549217e53273fcbe
styppo <mail@styp-rekowsky.de>
2017-04-20 19:35:59 -0600

    AccountsTree refactoring

--- src/consensus/accounts/AccountsTree.js
+++ src/consensus/accounts/AccountsTree.js
@@ -1,125 +1,159 @@
 // TODO: use firstchar of key as child index
-class AccountsTree {
-
-  constructor(db) {
-    this.db = db;
-  }
-
-  async put(key, value) {
-    if (!this.rootNode) {
-      this.rootNode = await this.db.put(['',false]);
+class AccountNode {
+    constructor(prefix, accountState) {
+        this.prefix = prefix;
+        this.accountState = accountState;
     }
-    let currNode = await this.db.get(this.rootNode);
-    let parentNodes = [];
-    let childIndex;
-    let nodeKey;
-    let nodeKeyLen;
+}
 
-    // traverse
-    traverse: while (true) {
-      nodeKey = currNode[0];
-      nodeKeyLen = nodeKey.length;
-      // Find common prefix
-      let commonPrefix = '';
-      let i = 0;
-      for (; i < nodeKeyLen; ++i) {
-        if (nodeKey[i] !== key[i]) break;
-        commonPrefix += nodeKey[i];
-      }
+class BranchNode {
+    constructor(prefix = new Uint8Array()) {
+        this.prefix = prefix;
+        this.children = [];
+    }
 
-      key = key.slice(i);
-      if (i !== nodeKeyLen) {
-        await this.db.delete(currNode);
-        currNode[0] = nodeKey.slice(i);
-        const newCurrKey = await this.db.put(currNode);
+    getChild(prefix) {
+        return this.children[prefix[0]];
+    }
 
-        const newChild = [key,value];
+    putChild(prefix, nodeKey) {
+        this.children[prefix[0]] = nodeKey;
+    }
+}
+
+class AccountsTree {
+    constructor(db) {
+        this._db = db;
+        this._rootKey = undefined;
+    }
+
+    async put(accountAddr, accountState) {
+        // Insert root node if the tree is (initially) empty
+        if (!this._rootKey) {
+            this._rootKey = await this.db.put(new BranchNode());
+        }
+
+        // Insert accountState into the tree at accoutAddr.
+        const rootNode = this.db.get(this._rootKey);
+        return await this._insert(rootNode, accountAddr, accountState, []);
+    }
+
+    async _insert(node, accountAddr, accountState, rootPath) {
+        // Find common prefix between node and new address.
+        const commonPrefix = AccountsTree._commonPrefix(node.prefix, accountAddr);
+
+        // Cut common prefix off the new address.
+        accountAddr = accountAddr.subarray(commonPrefix.length);
+
+        // If the node prefix does not fully match the new address, split the node.
+        if (commonPrefix.length !== node.prefix.length) {
+            // Cut the common prefix off the existing node.
+            await this.db.delete(node);
+            node.prefix = node.prefix.slice(i);
+            const nodeKey = await this.db.put(node);
+
+            // Insert the new account node.
+            const newChild = new AccountNode(accountAddr, accountState);
+            const newChildKey = await this.db.put(newChild);
+
+            // Insert the new parent node.
+            const newParent = new BranchNode(commonPrefix);
+            newParent.putChild(node.prefix, nodeKey);
+            newParent.putChild(newChild.prefix, newChildKey);
+            const newParentKey = await this.db.put(newParent);
+
+            return await this._updateKeys(newParent.prefix, newParentKey, rootPath);
+        }
+
+        // If the remaining address is empty, we have found an (existing) node
+        // with the given address. Update the account state.
+        if (!accountAddr.length) {
+            await this.db.delete(node);
+            node.accountState = accountState;
+            const nodeKey = await this.db.put(node);
+            return await this._updateKeys(node.prefix, nodeKey, rootPath);
+        }
+
+        // If the node prefix matches and there are address bytes left, descend into
+        // the matching child node if one exists.
+        const childKey = node.getChild(accountAddr);
+        if (child) {
+            const childNode = await this.db.get(childKey);
+            rootPath.push(node);
+            return await this._insert(childNode, accountAddr, accountState, rootPath);
+        }
+
+        // If no matching child exists, add a new child account node to the current node.
+        const newChild = new AccountNode(accountAddr, accountState);
         const newChildKey = await this.db.put(newChild);
 
-        const newParent = [commonPrefix,'',newCurrKey,newChildKey];
-        const newNodeKey = await this.db.put(newParent);
+        await this.db.delete(node);
+        node.putChild(newChild.prefix, newChildKey);
+        const nodeKey = await this.db.put(node);
 
-        return await this._traverseHome(newNodeKey, parentNodes);
-      }
-
-      if (!key.length) {
-        await this.db.delete(currNode);
-        currNode[1] = value;
-        const newNodeKey = await this.db.put(currNode);
-        return await this._traverseHome(newNodeKey, parentNodes);
-      }
-
-      //Find next node
-      i = 2;
-      for (; i < currNode.length; ++i) {
-        let nextNode = await this.db.get(currNode[i]);
-        if (nextNode[0][0] !== key[0]) continue;
-        parentNodes.push([currNode,i]);
-        currNode = nextNode;
-        continue traverse;
-      }
-
-      const newChild = [key,value];
-      const newChildKey = await this.db.put(newChild);
-      await this.db.delete(currNode);
-      currNode.push(newChildKey);
-      const newNodeKey = await this.db.put(currNode);
-      return await this._traverseHome(newNodeKey, parentNodes);
+        return await this._updateKeys(node.prefix, nodeKey, rootPath);
     }
-  }
 
-  async _traverseHome(newParentKey, parentNodes) {
-    let j = parentNodes.length - 1;
-    for (; j >= 0; --j) {
-      const parentNode = parentNodes[j][0];
-      const childIndex = parentNodes[j][1];
-      await this.db.delete(parentNode);
+    async _updateKeys(prefix, nodeKey, rootPath) {
+        let i = rootPath.length - 1;
+        for (; i >= 0; --i) {
+            const node = rootPath[i];
+            await this.db.delete(node);
 
-      parentNode[childIndex] = newParentKey;
-      newParentKey = await this.db.put(parentNode);
+            node.putChild(prefix, nodeKey);
+
+            nodeKey = await this.db.put(node);
+            prefix = node.prefix;
+        }
+
+        this._rootKey = nodeKey;
+        return this._rootKey;
     }
-    this.rootNode = newParentKey;
-    return this.rootNode;
-  }
 
-  async get(key) {
-    if (!this.rootNode) return;
-    let currNode = await this.db.get(this.rootNode);
-
-    traverse: while (true) {
-      const nodeKey = currNode[0];
-
-      // Find common prefix
-      let commonPrefix = '';
-      let i = 0;
-      for (; i < nodeKey.length; ++i) {
-        if (nodeKey[i] !== key[i]) break;
-        commonPrefix += nodeKey[i];
-      }
-      key = key.slice(i);
-
-      if (i !== nodeKey.length) return;
-
-      if (key === '') {
-        return currNode[1];
-      }
-
-      //Find next node
-      i = 2;
-      for (; i < currNode.length; ++i) {
-        let nextNode = await this.db.get(currNode[i]);
-        if (nextNode[0][0] !== key[0]) continue;
-        currNode = nextNode;
-        continue traverse;
-      }
-
-      return false;
+    async get(key) {
+        if (!this._rootKey) return;
+        const rootNode = await this.db.get(this._rootKey);
+        return await _retrieve(rootNode, accountAddr);
     }
-  }
 
-  get root() {
-    if (!this.rootNode)
-        return new Uint8Array(new ArrayBuffer(32));
-    return Buffer.fromBase64(this.rootNode);
-  }
+    async _retrieve(node, accountAddr) {
+        // Find common prefix between node and requested address.
+        const commonPrefix = AccountsTree._commonPrefix(node.prefix, accountAddr);
+
+        // If the prefix does not fully match, the requested address is not part
+        // of this node.
+        if (commonPrefix.length !== node.prefix.length) return false;
+
+        // Cut common prefix off the new address.
+        accountAddr = accountAddr.subarray(commonPrefix.length);
+
+        // If the address remaining address is empty, we have found the requested
+        // node.
+        if (!accountAddr.length) return node.accountState;
+
+        // Descend into the tree if a matching child node exists
+        const childKey = node.getChild(accountAddr);
+        if (child) {
+          const childNode = await this.db.get(childKey);
+          return await this._retrieve(childNode, accountAddr);
+        }
+
+        // No matching child exists, the requested address is not part of this node.
+        return false;
+    }
+
+    static _commonPrefix(arr1, arr2) {
+        let commonPrefix = new Uint8Array(arr1.length);
+        let i = 0;
+        for (; i < arr1.length; ++i) {
+          if (arr1[i] !== arr2[i]) break;
+          commonPrefix[i]= arr1[i];
+        }
+        return commonPrefix.slice(0, i);
+    }
+
+    get hash() {
+        if (!this._rootKey) return new Hash();
+        return Hash.fromBase64(this._rootKey);
+    }
 }
