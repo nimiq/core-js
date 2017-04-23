c42143e3ef14417ff277060cb95b28230ba7450e
Your Name <robin@stetsolutions.com>
2017-04-22 21:29:25 -0600

    Refine specs

--- specs/consensus/accounts/AccountsTree.spec.js
+++ specs/consensus/accounts/AccountsTree.spec.js
@@ -61,17 +61,17 @@ describe('AccountsTree', () => {
             await tree.put(address2, balance2);
             await tree.put(address3, balance3);
 
-            const balance1_test = await tree.get(address1);
-            expect(balance1_test.value).toEqual(value1);
-            expect(balance1_test.nonce).toEqual(nonce1);
+            const balanceTest1 = await tree.get(address1);
+            expect(balanceTest1.value).toEqual(value1);
+            expect(balanceTest1.nonce).toEqual(nonce1);
 
-            const balance2_test = await tree.get(address2);
-            expect(balance2_test.value).toEqual(value2);
-            expect(balance2_test.nonce).toEqual(nonce2);
+            const balanceTest2 = await tree.get(address2);
+            expect(balanceTest2.value).toEqual(value2);
+            expect(balanceTest2.nonce).toEqual(nonce2);
 
-            const balance3_test = await tree.get(address3);
-            expect(balance3_test.value).toEqual(value3);
-            expect(balance3_test.nonce).toEqual(nonce3);
+            const balanceTest3 = await tree.get(address3);
+            expect(balanceTest3.value).toEqual(value3);
+            expect(balanceTest3.nonce).toEqual(nonce3);
 
             done();
         }
@@ -104,6 +104,75 @@ describe('AccountsTree', () => {
         test();
     });
 
+    it('root hash is invariant to insertion order', (done) => {
+        const value1 = 8;
+        const nonce1 = 8;
+        const balance1 = new Balance(value1, nonce1);
+        const address1 = new Address(Dummy.address1);
+
+        const value2 = 88;
+        const nonce2 = 88;
+        const balance2 = new Balance(value2, nonce2);
+        const address2 = new Address(Dummy.address2);
+
+        const value3 = 88888888;
+        const nonce3 = 88888888;
+        const balance3 = new Balance(value3, nonce3);
+        const address3 = new Address(Dummy.address3);
+
+        const tree = new AccountsTree();
+
+        async function test(){
+
+            // order1
+            await tree.put(address1, balance1);
+            await tree.put(address2, balance1);
+            await tree.put(address3, balance1);
+            const state1 = tree.root.toBase64();
+
+
+            // "reset"
+            await tree.put(address1, balance2);
+            await tree.put(address3, balance2);
+            await tree.put(address2, balance2);
+            // order2
+            await tree.put(address1, balance1);
+            await tree.put(address3, balance1);
+            await tree.put(address2, balance1);
+            const state2 = tree.root.toBase64();
+
+            
+            // "reset"
+            await tree.put(address1, balance2);
+            await tree.put(address3, balance2);
+            await tree.put(address2, balance2);
+            // order3
+            await tree.put(address2, balance1);
+            await tree.put(address1, balance1);
+            await tree.put(address3, balance1);
+            const state3 = tree.root.toBase64();
+
+
+            // "reset"
+            await tree.put(address1, balance2);
+            await tree.put(address3, balance2);
+            await tree.put(address2, balance2);
+            // order4
+            await tree.put(address2, balance1);
+            await tree.put(address3, balance1);
+            await tree.put(address1, balance1);
+            const state4 = tree.root.toBase64();
+
+            expect(state2).toBe(state1);
+            expect(state3).toBe(state1);
+            expect(state4).toBe(state1);
+
+            done();
+        }
+
+        test();
+    });
+
 
     it('can handle concurrency', (done) => {
         const value1 = 8;
@@ -128,21 +197,23 @@ describe('AccountsTree', () => {
             tree.put(address2, balance2);
             tree.put(address3, balance3);
 
-            const balance1_test = await tree.get(address1);
-            expect(balance1_test.value).toEqual(value1);
-            expect(balance1_test.nonce).toEqual(nonce1);
+            const balanceTest1 = tree.get(address1);
+            expect(balanceTest1.value).toEqual(value1);
+            expect(balanceTest1.nonce).toEqual(nonce1);
 
-            const balance2_test = await tree.get(address2);
-            expect(balance2_test.value).toEqual(value2);
-            expect(balance2_test.nonce).toEqual(nonce2);
+            const balanceTest2 = tree.get(address2);
+            expect(balanceTest2.value).toEqual(value2);
+            expect(balanceTest2.nonce).toEqual(nonce2);
 
-            const balance3_test = await tree.get(address3);
-            expect(balance3_test.value).toEqual(value3);
-            expect(balance3_test.nonce).toEqual(nonce3);
+            const balanceTest3 = tree.get(address3);
+            expect(balanceTest3.value).toEqual(value3);
+            expect(balanceTest3.nonce).toEqual(nonce3);
 
             done();
         }
 
         test();
     });
+
+
 });
