7353aaaa8b116248e02c863b13c29f694f1b6293
Your Name <robin@stetsolutions.com>
2017-04-22 23:48:24 -0600

    Refine P2PClients spec

--- specs/p2p/P2PClient.spec.js
+++ specs/p2p/P2PClient.spec.js
@@ -27,7 +27,30 @@ describe('P2PClient',() => {
 		client.notfound([vec1]);
 	})
 
+	it('can send a GetDataP2PMessage', (done) => {
+		const spy = new SpyP2PChannel( msg => {
+				const getDataMsg = GetDataP2PMessage.unserialize(msg);
+        		expect(getDataMsg.count).toBe(1);
+        		expect(getDataMsg.vectors[0].equals(vec1)).toBe(true);
+				done();
+			});
+		const client = new P2PClient(spy);
+		client.getdata([vec1]);
+	})
+
+	it('can send a BlockP2PMessage', (done) => {
+		const spy = new SpyP2PChannel( msg => {
+				const blockMsg = BlockP2PMessage.unserialize(msg);
+        		expect(blockMsg.header.equals(Dummy.block1.header)).toBe(true);
+        		expect(blockMsg.body.equals(Dummy.block1.body)).toBe(true);
+				done();
+			});
+		const client = new P2PClient(spy);
+		client.block(Dummy.block1);
+	})
+
 	it('can receive a InvP2PMessage', (done) => {
+		const message = new InvP2PMessage(count, [vec1]);
 		const spy = new SpyP2PChannel();
 		const client = new P2PClient(spy);
 		
@@ -40,36 +63,38 @@ describe('P2PClient',() => {
 	})
 
 	it('can receive a NotFoundP2PMessage', (done) => {
+		const message = new NotFoundP2PMessage(count, [vec1]);
 		const spy = new SpyP2PChannel();
 		const client = new P2PClient(spy);
 		
-		client.on(message.type, invMsgTest => {
-			expect(invMsgTest.count).toBe(count);
-        	expect(invMsgTest.vectors[0].equals(vec1)).toBe(true);
+		client.on(message.type, notfoundMsgTest => {
+			expect(notfoundMsgTest.count).toBe(count);
+        	expect(notfoundMsgTest.vectors[0].equals(vec1)).toBe(true);
 			done();
 		});
 		spy.fire('message', message.serialize());
 	});
 
 	it('can receive a GetDataP2PMessage', (done) => {
+		const message = new GetDataP2PMessage(count, [vec1]);
 		const spy = new SpyP2PChannel();
 		const client = new P2PClient(spy);
 		
-		client.on(message.type, invMsgTest => {
-			expect(invMsgTest.count).toBe(count);
-        	expect(invMsgTest.vectors[0].equals(vec1)).toBe(true);
+		client.on(message.type, getDataMsgTest => {
+			expect(getDataMsgTest.count).toBe(count);
+        	expect(getDataMsgTest.vectors[0].equals(vec1)).toBe(true);
 			done();
 		});
 		spy.fire('message', message.serialize());
 	});
 
 	it('can receive a BlockP2PMessage', (done) => {
+		const message = new BlockP2PMessage(Dummy.block1);
 		const spy = new SpyP2PChannel();
 		const client = new P2PClient(spy);
-		
-		client.on(message.type, invMsgTest => {
-			expect(invMsgTest.count).toBe(count);
-        	expect(invMsgTest.vectors[0].equals(vec1)).toBe(true);
+		client.on(message.type, blockMsgTest => {
+			expect(blockMsgTest.count).toBe(count);
+        	expect(blockMsgTest.vectors[0].equals(vec1)).toBe(true);
 			done();
 		});
 		spy.fire('message', message.serialize());
