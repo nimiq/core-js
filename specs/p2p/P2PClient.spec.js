8b6269190bb251e1f6c64e7e150a2fe131be6bfe
Your Name <robin@stetsolutions.com>
2017-04-22 23:27:35 -0600

    Refine P2PClients spec

--- specs/p2p/P2PClient.spec.js
+++ specs/p2p/P2PClient.spec.js
@@ -3,10 +3,7 @@ describe('P2PClient',() => {
 	const hash = new Hash(Dummy.hash1);
     const vec1 = new InvVector(type,hash);
 	const count = 1;
-    const vectorType = 888;
-    const vectorHash = new Hash(Dummy.hash2);
-    const vector1 = new InvVector(vectorType, vectorHash);
-	const message = new InvP2PMessage(count, [vector1]);
+	const message = new InvP2PMessage(count, [vec1]);
 
 	it('can send a InvP2PMessage', (done) => {
 		const spy = new SpyP2PChannel( msg => {
@@ -60,7 +57,7 @@ describe('P2PClient',() => {
 		
 		client.on(message.type, invMsgTest => {
 			expect(invMsgTest.count).toBe(count);
-        	expect(invMsgTest.vectors[0].equals(vector1)).toBe(true);
+        	expect(invMsgTest.vectors[0].equals(vec1)).toBe(true);
 			done();
 		});
 		spy.fire('message', message.serialize());
