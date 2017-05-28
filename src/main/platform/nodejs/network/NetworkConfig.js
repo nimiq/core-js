634f15e95710f3fc1f5f5c77a54d467543e6abe3
styppo <mail@styp-rekowsky.de>
2017-05-27 23:36:02 -0600

    Fix NodeJS NetworkConfig

--- src/main/platform/nodejs/network/NetworkConfig.js
+++ src/main/platform/nodejs/network/NetworkConfig.js
@@ -4,7 +4,7 @@ class NetworkConfig {
             throw 'PeerAddress is not configured.';
         }
 
-        return new WssNetAddress(
+        return new WssPeerAddress(
             Services.myServices(), Date.now(),
             NetworkConfig._myHost, NetworkConfig._myPort);
     }
