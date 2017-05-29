b25c65e19069cbd53438437a4984c76e83c7e27d
styppo <mail@styp-rekowsky.de>
2017-05-29 17:48:14 -0600

    Add connect timeout to WebSocketConnector (NodeJS)

--- src/main/platform/nodejs/network/websocket/WebSocketConnector.js
+++ src/main/platform/nodejs/network/websocket/WebSocketConnector.js
@@ -21,15 +21,42 @@ class WebSocketConnector extends Observable {
         this._wss = new WebSocket.Server({server: httpsServer});
         this._wss.on('connection', ws => this._onConnection(ws, null));
 
+        this._timers = new Timers();
+
         console.log('WebSocketConnector listening on port ' + port);
     }
 
     connect(peerAddress) {
         if (peerAddress.protocol !== Protocol.WS) throw 'Malformed peerAddress';
 
+        const timeoutKey = 'connect_' + peerAddress;
+        if (this._timers.timeoutExists(timeoutKey)) {
+            console.warn('WsConnector: Already connecting to ' + peerAddress);
+            return false;
+        }
+
         const ws = new WebSocket('wss://' + peerAddress.host + ':' + peerAddress.port);
-        ws.onopen = () => this._onConnection(ws, peerAddress);
-        ws.onerror = e => this.fire('error', peerAddress, e);
+        ws.onopen = () => {
+            this._timers.clearTimeout(timeoutKey);
+
+            const netAddress = NetAddress.fromHostname(peerAddress.host, peerAddress.port);
+            const conn = new PeerConnection(ws, Protocol.WS, netAddress, peerAddress);
+            this.fire('connection', conn);
+        };
+        ws.onerror = e => {
+            this.fire('error', peerAddress, e);
+            this._timers.clearTimeout(timeoutKey);
+        };
+
+        this._timers.setTimeout(timeoutKey, () => {
+            this.fire('error', peerAddress);
+            this._timers.clearTimeout(timeoutKey);
+
+            // We don't want to fire the error event again if the websocket
+            // connect fails at a later time.
+            ws.onerror = null;
+        }, WebSocketConnector.CONNECT_TIMEOUT);
+
         return true;
     }
 
