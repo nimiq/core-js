b1bcc055c0df9a78e857dd65c892a640ace7bad1
Marvin W <git@larma.de>
2017-05-31 17:54:44 -0600

    Fix Log storage in browsers, small change in output (#81)

--- src/main/platform/browser/utils/LogNative.js
+++ src/main/platform/browser/utils/LogNative.js
@@ -3,8 +3,13 @@ class LogNative {
         this._global_level = Log.DEBUG;
         this._tag_levels = {};
         if (window.localStorage) {
-            const c = window.localStorage.getItem('log_tag_levels');
-            if (c) this._tag_levels = c;
+            try {
+                let c = window.localStorage.getItem('log_tag_levels');
+                if (c && typeof c === 'string') c = JSON.parse(c);
+                if (c && typeof c === 'object') this._tag_levels = c;
+            } catch (e) {
+                console.warn('Failed to load log configuration from local storage.');
+            }
         }
     }
 
@@ -12,21 +17,25 @@ class LogNative {
         if (tag && this._tag_levels[tag]) {
             return this._tag_levels[tag] <= level;
         }
+        if (this._tag_levels['*']) {
+            return this._tag_levels['*'] <= level;
+        }
         return this._global_level <= level;
     }
 
     setLoggable(tag, level) {
+        if (tag && tag.name) tag = tag.name;
         this._tag_levels[tag] = level;
         if (window.localStorage) {
-            window.localStorage.setItem('log_tag_levels', this._tag_levels);
+            window.localStorage.setItem('log_tag_levels', JSON.stringify(this._tag_levels));
         }
     }
 
     msg(level, tag, args) {
-        if (!this.isLoggable(tag, level)) return;
         if (tag && tag.name) tag = tag.name;
+        if (!this.isLoggable(tag, level)) return;
         if (tag) args.unshift(tag + ':');
-        args.unshift(`[${new Date().toTimeString().substr(0, 8)}:${Log._level_tag(level)}]`);
+        args.unshift(`[${new Date().toTimeString().substr(0, 8)}|${Log._level_tag(level)}]`);
         if (console.error && level >= Log.ERROR) {
             console.error.apply(null, args);
         } else if (console.warn && level >= Log.WARNING) {
