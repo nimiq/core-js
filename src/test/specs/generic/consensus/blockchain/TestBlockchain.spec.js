76564644cb5056496e43217a35bba49e4987dbf9
Oliver Schranz <development@oliverschranz.de>
2017-05-31 16:36:13 +0200

    Refactored TestBlockchain: parameterized and customizable block and transaction creation
    It is now possible to create specialized (and also invalid) transactions and block.
    Also, moved the static user generator method to the TestBlockchain class.

--- src/test/specs/generic/consensus/blockchain/TestBlockchain.spec.js
+++ src/test/specs/generic/consensus/blockchain/TestBlockchain.spec.js
@@ -1,5 +1,14 @@
 class TestBlockchain extends Blockchain {
 
+    static get MAX_NUM_TRANSACTIONS() {
+        return Math.floor(              // round off
+            (Policy.BLOCK_SIZE_MAX  -   // block size limit
+            116 -                       // header size
+            20) /                       // miner address size
+            165);                       // transaction size
+
+    }
+
     constructor(store, accounts, blocks, users) {
         const thisPromise = super(store, accounts);
 
@@ -23,22 +32,53 @@ class TestBlockchain extends Blockchain {
         return this._users;
     }
 
-    async createBlock() {
+    static async createTransaction(senderPubKey, receiverAddr, amount = 1, fee = 1, nonce = 0,
+                                   senderPrivKey = undefined, signature = undefined) {
+        // console.info('Creating transaction from sender ' + senderPubKey + ' to address ' + receiverAddr + '. Amount: '
+        //     + amount + ', fee: ' + fee + ', nonce: ' + nonce);
+
+        const transaction = new Transaction(senderPubKey, receiverAddr, amount, fee, nonce);
+
+        // allow to hardcode a signature
+        if (!signature) {
+            // if no signature is provided, the secret key is required
+            if (!senderPrivKey) {
+                throw 'Signature computation requested, but no sender private key provided';
+            }
+            // console.info('Signing transaction with provided secret key');
+            const internalPrivate = await Crypto.importPrivate(senderPrivKey);
+
+            signature = await Crypto.sign(internalPrivate, transaction.serializeContent());
+        }
+        transaction.signature = signature;
+        return transaction;
+    }
+
+    // TODO can still run into balance problems: block height x and subsequent `mining` means that only the first x
+    // users are guaranteed to have a non-zero balance. Depending on the existing transactions, this can improve a bit...
+    async generateTransactions(numTransactions, noDupliateSenders = true, sizeLimit = true) {
         const transactions = [];
 
-        // index of block in the chain, genesis is omitted
-        const blockIndex = this.height - 1;
+        const numUsers = this.users.length;
 
-        /*  Note on transactions and balances:
+        if (noDupliateSenders && numTransactions > numUsers) {
+            // only one transaction per user
+            console.warn('Reducing transactions to ' + numTransactions + ' to avoid sender duplication.');
+            numTransactions = numUsers;
+        }
+
+        if (sizeLimit && numTransactions > TestBlockchain.MAX_NUM_TRANSACTIONS) {
+            console.warn('Reducing transactions to ' + numTransactions + ' to avoid exceeding the size limit.');
+            numTransactions = TestBlockchain.MAX_NUM_TRANSACTIONS;
+        }
+
+         /* Note on transactions and balances:
          We fill up the balances of users in increasing order, therefore the size of the chain determines how many
          users already have a non-zero balance. Hence, for block x, all users up to user[x] have a non-zero balance.
          At the same time, there must not be more than one transaction from the same sender.
          */
-        const numUsers = this.users.length;
-        const numTransactions = Math.min(this.height, numUsers);
         for (let j = 0; j < numTransactions; j++) {
             const sender = this.users[j % numUsers];
-
             const senderAddress = new Address(sender.address);
             const senderPubKey = new PublicKey(sender.publicKey);
             const receiver = this.users[(j + 1) % numUsers];
@@ -46,41 +86,51 @@ class TestBlockchain extends Blockchain {
 
             // 10% transaction + 5% fee
             const balanceValue = (await this.accounts.getBalance(senderAddress)).value;
-
             const amount = Math.floor(balanceValue / 10);
             const fee = Math.floor(amount / 2);
             const nonce = j;
 
-            const transaction = new Transaction(senderPubKey,
-                receiverAddress, amount, fee, nonce,
-                undefined);
-
-            // signing transaction
-            const internalPrivate = await Crypto.importPrivate(sender.privateKey);
-            const signature = await Crypto.sign(internalPrivate, transaction.serializeContent());
-            transaction.signature = signature;
+            const transaction = await TestBlockchain.createTransaction(senderPubKey, receiverAddress, amount, fee,
+                nonce, sender.privateKey);
 
             transactions.push(transaction);
         }
 
-        const prevHash = this.headHash;
-        const accountsHash = new Hash(await this._accounts.hash());
+        return transactions;
+    }
 
-        const miner = this.users[(blockIndex + 1) % numUsers];     // user[0] created genesis, hence we start with user[1]
+    async createBlock(transactions, prevHash, accountsHash, bodyHash, miner, difficulty, timestamp, nonce, mockPoW = true) {
+
+
+        // index of block in the chain, genesis is omitted
+        const blockIndex = this.height - 1;
+
+        const numUsers = this.users.length;
+
+        // create transactions
+        if (!transactions) {
+            transactions = await this.generateTransactions(this.height);
+        }
+
+        prevHash = prevHash || this.headHash;
+        accountsHash = accountsHash || new Hash(await this._accounts.hash());
+        miner = miner || this.users[(blockIndex + 1) % numUsers];     // user[0] created genesis, hence we start with user[1]
         const body = await new BlockBody(new Address(miner.address), transactions);
-        const bodyHash = new Hash(await body.hash());
-        const difficulty = await
-            this.getNextCompactTarget();
-        const header = await
-            new BlockHeader(prevHash, bodyHash, accountsHash,
-                difficulty, blockIndex + 1, blockIndex + 1);
+
+        bodyHash = bodyHash || new Hash(await body.hash());
+        difficulty = difficulty || await this.getNextCompactTarget();
+        timestamp = timestamp || ((blockIndex + 1) * 10);
+        nonce = nonce || blockIndex + 1;
+        const header = await new BlockHeader(prevHash, bodyHash, accountsHash, difficulty, timestamp, nonce);
         const block = await new Block(header, body);
 
-        // mock (!) proof of work b/c we cannot really mine here
-        block.header.verifyProofOfWork = (buf) => {
-            // console.log('Faking PoW');
-            return true;
-        };
+        if (mockPoW) {
+            // mock (!) proof of work b/c we cannot really mine here
+            block.header.verifyProofOfWork = (buf) => {
+                // console.log('Faking PoW');
+                return true;
+            };
+        }
 
         return block;
     }
@@ -125,5 +175,28 @@ class TestBlockchain extends Blockchain {
         return testBlockchain;
     }
 
+    // DEBUG
+
+    static async generateUsers(num) {
+
+        const users = [];
+
+        for (let i = 0; i < num; i++) {
+            const keys = await Crypto.generateKeys();
+            const privateKey = await Crypto.exportPrivate(keys.privateKey);
+            const publicKey = await Crypto.exportPublic(keys.publicKey);
+            const address = await Crypto.publicToAddress(publicKey);
+
+            users.push({
+                // 'keys': keys,               // raw format as generated by crypto lib
+                'privateKey': privateKey,   // exported jwt format
+                'publicKey': publicKey,     // base64 of raw export
+                'address': address          // base64
+            });
+            // console.info('Created user ' + i);
+
+        }
+        return users;
+    }
 }
 Class.register(TestBlockchain);
