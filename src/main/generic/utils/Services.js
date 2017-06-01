d49916d51da9365b140d8e5fd2381c2eee9b559a
pb <berrang@cs.uni-saarland.de>
2017-06-01 16:02:42 -0600

    fix service mask

--- src/main/generic/utils/Services.js
+++ src/main/generic/utils/Services.js
@@ -1,7 +1,8 @@
 class Services {
     // XXX Temporary stub, needs to be configurable later on.
     static myServices() {
-        return 0;
+        // Needs to be != 0 in order to be discoverable by peers.
+        return Services.DEFAULT;
     }
 
     // Used for filtering peer addresses by services.
@@ -9,4 +10,5 @@ class Services {
         return 0xffffffff;
     }
 }
+Services.DEFAULT = 1;
 Class.register(Services);
