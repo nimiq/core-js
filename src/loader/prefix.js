3b3f4aa22bb491549f4b5fd4890fcf5db61c2a4d
Marvin W <git@larma.de>
2017-06-01 20:55:46 -0600

    Travis CI support

--- src/loader/prefix.js
+++ src/loader/prefix.js
@@ -1,5 +1,5 @@
-
-(function(exports) {
+Nimiq = typeof Nimiq !== 'undefined' ? Nimiq : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : {};
+(function (exports) {
     exports = typeof exports !== 'undefined' ? exports : {};
 
 
