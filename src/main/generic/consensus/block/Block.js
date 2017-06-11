be15369d2679076b3b7def13e6291c45e8bf6453
pb <mail@paberr.net>
2017-06-12 00:25:20 +0200

    introduce checkpoints that allow fast-forwarding of the blockchain first checkpoint is at block height 5801

--- src/main/generic/consensus/block/Block.js
+++ src/main/generic/consensus/block/Block.js
@@ -103,4 +103,25 @@ Block.GENESIS.hash().then(hash => {
     Block.GENESIS.HASH = hash;
     //Object.freeze(Block.GENESIS);
 });
+
+/* Checkpoint Block */
+Block.CHECKPOINT = new Block(
+    new BlockHeader(
+        new Hash(BufferUtils.fromBase64('AAAAAtSCUsJPWRSU3bBdWO05QKWjfyww5LhdAg01QC4=')),
+        new Hash(BufferUtils.fromBase64('DTYnX+Vf1ps/vA9PWBy0raEcQOCqjUy4N5H/0AcP54w=')),
+        new Hash(BufferUtils.fromBase64('lVAOIc4MdxuXM472DoF2hPbolmYIcOxUNXE2zwvs5XE=')),
+        488323620,
+        5801,
+        1497166215,
+        1554547,
+        1),
+    new BlockBody(new Address(BufferUtils.fromBase64('mOoPdUsZHV0hcd7js9SFKPZH/wI=')), [
+        Transaction.unserialize(BufferUtils.fromBase64('AAEAtKYJ6YL6xGiv6b5DvFVbp7QYAKa+QG1bRdftz6aqqce6kbU79DdvzMq8GJs8yFtLBcCGSr51U7L1QrmSFOLcysLQnv5fnlAOfKNKOjyTTSTiUFOgQfKgXyAAAAAAAAAAAAAAAAAAAAQ8OQFTMQWD4pp7SSBoVICMLUv82mqYG47w7/IBMwqz9D9NH0aG+QNqMHE9tZHkFbTCtQQUpZ861AKQdy4eG2ul'))
+    ])
+);
+Block.CHECKPOINT.hash().then(hash => {
+    Block.CHECKPOINT.HASH = hash;
+    //Object.freeze(Block.GENESIS);
+});
+//Block.CHECKPOINT.TOTAL_WORK = 17711565.212913718;
 Class.register(Block);
