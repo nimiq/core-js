9af3cbc8b2011127ba1410928019b969e0ab70ce
pb <mail@paberr.net>
2017-06-14 01:40:32 +0200

    type error

--- src/main/generic/consensus/block/Block.js
+++ src/main/generic/consensus/block/Block.js
@@ -122,5 +122,5 @@ Block.CHECKPOINT.hash().then(hash => {
     //Object.freeze(Block.GENESIS);
 });
 Block.CHECKPOINT.TOTAL_WORK = 22921810.269859694;
-Block.OLD_CHECKPOINTS = new IndexedArray([BufferUtils.fromBase64('AAAACxKJIIfQb99dTIuiRyY6VkRlzBfbyknKo/515Ho=')]);
+Block.OLD_CHECKPOINTS = new IndexedArray([new Hash(BufferUtils.fromBase64('AAAACxKJIIfQb99dTIuiRyY6VkRlzBfbyknKo/515Ho='))]);
 Class.register(Block);
