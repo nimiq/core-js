acd40a101e66839c2f0d22e595c32f7afe0bde83
Marvin W <git@larma.de>
2017-06-16 17:42:44 +0200

    Don't use toString with radix as it's implementation-dependant behavior.
    See ECMA-262 6, 20.1.3.6 [1] step 9.
    
    [1] http://www.ecma-international.org/ecma-262/6.0/#sec-tostring-applied-to-the-number-type

--- src/main/generic/consensus/block/BlockUtils.js
+++ src/main/generic/consensus/block/BlockUtils.js
@@ -4,29 +4,36 @@ class BlockUtils {
     }
 
     static targetToCompact(target) {
-        // Convert the target into base 16 with zero-padding.
-        let base16 = target.toString(16);
-        if (base16.length % 2 != 0) {
-            base16 = "0" + base16;
+        if (!Number.isFinite(target) || Number.isNaN(target)) throw 'Invalid Target';
+
+        // Divide to get first byte
+        let tmp = target;
+        let size = 1;
+        while (tmp >= 256) {
+            size++;
+            tmp /= 256;
         }
+
+        let i = 0;
 
         // If the first (most significant) byte is greater than 127 (0x7f),
         // prepend a zero byte.
-        if (parseInt(base16.substr(0, 2), 16) > 0x7f) {
-            base16 = "00" + base16;
+        if (tmp >= 0x80) {
+            size++;
+            i++;
         }
 
         // The first byte of the 'compact' format is the number of bytes,
         // including the prepended zero if it's present.
-        let size = base16.length / 2;
         let compact = size << 24;
 
         // The following three bytes are the first three bytes of the above
         // representation. If less than three bytes are present, then one or
         // more of the last bytes of the compact representation will be zero.
         const numBytes = Math.min(size, 3);
-        for (let i = 0; i < numBytes; ++i) {
-            compact |= parseInt(base16.substr(i * 2, 2), 16) << ((2 - i) * 8);
+        for (; i < numBytes; ++i) {
+            compact |= (Math.floor(tmp) % 256) << ((2 - i) * 8);
+            tmp *= 256;
         }
 
         return compact;
@@ -37,7 +44,7 @@ class BlockUtils {
     }
 
     static difficultyToCompact(difficulty) {
-        return BlockUtils.targetToCompact(Policy.BLOCK_TARGET_MAX / difficulty);
+        return BlockUtils.targetToCompact(BlockUtils.difficultyToTarget(difficulty));
     }
 
     static difficultyToTarget(difficulty) {
--- src/test/specs/generic/consensus/block/BlockUtils.spec.js
+++ src/test/specs/generic/consensus/block/BlockUtils.spec.js
@@ -1,10 +1,5 @@
 describe('BlockUtils', () => {
     it('computes valid targets', () => {
-        // let difficulty = 0;
-        // let target = BlockUtils.difficultyToTarget(difficulty);
-
-        // expect(BlockUtils.isValidTarget(target)).toBe(true);
-
         let difficulty = 1;
         let target = BlockUtils.difficultyToTarget(difficulty);
 
@@ -32,13 +27,8 @@ describe('BlockUtils', () => {
     });
 
     it('computes valid compacts', () => {
-        let difficulty = 0;
+        let difficulty = 1;
         let target = BlockUtils.difficultyToCompact(difficulty);
-
-        expect(BlockUtils.isValidCompact(target)).toBe(true);
-
-        difficulty = 1;
-        target = BlockUtils.difficultyToCompact(difficulty);
 
         expect(BlockUtils.isValidCompact(target)).toBe(true);
 
@@ -61,5 +51,32 @@ describe('BlockUtils', () => {
         target = BlockUtils.difficultyToCompact(difficulty);
 
         expect(BlockUtils.isValidCompact(target)).toBe(true);
+    });
+
+    it('computes correct compacts', () => {
+        let difficulty = 1;
+        let target = BlockUtils.difficultyToCompact(difficulty);
+
+        expect(target).toEqual(520159231);
+
+        difficulty = 250;
+        target = BlockUtils.difficultyToCompact(difficulty);
+
+        expect(target).toEqual(503383587);
+
+        difficulty = NumberUtils.UINT32_MAX;
+        target = BlockUtils.difficultyToCompact(difficulty);
+
+        expect(target).toEqual(453050367);
+
+        difficulty = NumberUtils.UINT64_MAX;
+        target = BlockUtils.difficultyToCompact(difficulty);
+
+        expect(target).toEqual(403177464);
+
+        difficulty = Policy.BLOCK_TARGET_MAX;
+        target = BlockUtils.difficultyToCompact(difficulty);
+
+        expect(target).toEqual(16842752);
     });
 });
