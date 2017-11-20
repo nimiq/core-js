97af034da84fe4bb33551c3c49e714a27540a897
Marvin W <git@larma.de>
2017-11-20 13:56:48 +0100

    Node Client: no longer using Core object, fix wallet seed

--- clients/nodejs/index.js
+++ clients/nodejs/index.js
@@ -8,7 +8,7 @@ if (!argv.host || !argv.port || !argv.key || !argv.cert) {
 
 const host = argv.host;
 const port = parseInt(argv.port);
-const miner = argv.miner;
+const minerOptions = argv.miner;
 const passive = argv.passive;
 const key = argv.key;
 const cert = argv.cert;
@@ -27,60 +27,60 @@ if (argv['log-tag']) {
     });
 }
 
-console.log(`Nimiq NodeJS Client starting (host=${host}, port=${port}, miner=${!!miner}, passive=${!!passive})`);
+console.log(`Nimiq NodeJS Client starting (host=${host}, port=${port}, miner=${!!minerOptions}, passive=${!!passive})`);
 
 // XXX Configure Core.
 // TODO Create config/options object and pass to Core.get()/init().
 Nimiq.NetworkConfig.configurePeerAddress(host, port);
 Nimiq.NetworkConfig.configureSSL(key, cert);
 
-const options = {
-    'walletSeed': walletSeed
-};
+const TAG = 'Node';
 
-try {
-    (new Nimiq.Core(options)).then($ => {
-        Nimiq.Log.i(Nimiq.Core, `Blockchain: height=${$.blockchain.height}, totalWork=${$.blockchain.totalWork}, headHash=${$.blockchain.headHash.toBase64()}`);
+(async () => {
+    const consensus = await Nimiq.Consensus.full();
 
-        $.blockchain.on('head-changed', (head) => {
-            Nimiq.Log.i(Nimiq.Core, `Now at block: ${head.height}`);
-        });
+    const blockchain = consensus.blockchain;
+    const accounts = blockchain.accounts;
+    const mempool = consensus.mempool;
+    const network = consensus.network;
 
-        if (!passive) {
-            $.network.connect();
-        }
+    const wallet = walletSeed ? await Nimiq.Wallet.load(walletSeed) : await Nimiq.Wallet.getPersistent();
+    Nimiq.Log.i(TAG, `Wallet initialized for address ${wallet.address.toUserFriendlyAddress()}.`);
+    //Nimiq.Log.i(TAG, `Wallet initialized with seed ${wallet.dump()}.`);
 
-        if (miner) {
-            $.consensus.on('established', () => $.miner.startWork());
-            $.consensus.on('lost', () => $.miner.stopWork());
-            if (typeof miner === 'number') {
-                $.miner.threads = miner;
-            } else if (typeof miner === 'string') {
-                const margs = miner.split(':');
-                $.miner.threads = parseInt(margs[0]);
-                if (margs.length >= 2) { $.miner.throttleAfter = parseInt(margs[1]) * $.miner.threads; }
-                if (margs.length >= 3) { $.miner.throttleWait = parseInt(margs[2]); }
-            }
-            if (passive) {
-                $.miner.startWork();
-            }
-        }
+    const miner = new Nimiq.Miner(blockchain, mempool, wallet.address);
 
-        $.consensus.on('established', () => {
-            Nimiq.Log.i(Nimiq.Core, 'Blockchain consensus established');
-        });
+    Nimiq.Log.i(TAG, `Blockchain: height=${blockchain.height}, totalWork=${blockchain.totalWork}, headHash=${blockchain.headHash.toBase64()}`);
 
-        $.miner.on('block-mined', (block) => {
-            Nimiq.Log.i(Nimiq.Core, `Block mined: ${block.header}`);
-        });
+    blockchain.on('head-changed', (head) => {
+        Nimiq.Log.i(TAG, `Now at block: ${head.height}`);
     });
-} catch (code) {
-    switch (code) {
-        case Nimiq.Wallet.ERR_INVALID_WALLET_SEED:
-            console.log('Invalid wallet seed');
-            break;
-        default:
-            console.log('Nimiq initialization error');
-            break;
+
+    if (!passive) {
+        network.connect();
     }
-}
+
+    if (minerOptions) {
+        consensus.on('established', () => miner.startWork());
+        consensus.on('lost', () => miner.stopWork());
+        if (typeof minerOptions === 'number') {
+            miner.threads = minerOptions;
+        } else if (typeof minerOptions === 'string') {
+            const margs = minerOptions.split(':');
+            miner.threads = parseInt(margs[0]);
+            if (margs.length >= 2) { miner.throttleAfter = parseInt(margs[1]) * miner.threads; }
+            if (margs.length >= 3) { miner.throttleWait = parseInt(margs[2]); }
+        }
+        if (passive) {
+            miner.startWork();
+        }
+    }
+
+    consensus.on('established', () => {
+        Nimiq.Log.i(TAG, 'Blockchain consensus established');
+    });
+
+    miner.on('block-mined', (block) => {
+        Nimiq.Log.i(TAG, `Block mined: ${block.header}`);
+    });
+})().then(console.log, console.error);
