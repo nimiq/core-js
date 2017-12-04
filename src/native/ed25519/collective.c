8fcee98c5fdd61c35101d6b5f5f1db5e52faee5c
pb <mail@paberr.net>
2017-12-04 17:08:03 -0600

    Implement base functions for collective signing.

---
+++ src/native/ed25519/collective.c
@@ -0,0 +1,77 @@
+#include "ed25519.h"
+#include "ge.h"
+#include "sc.h"
+
+/*
+ * Necessary tools for collective signatures.
+ * Our implementation is inspired by:
+ * https://tools.ietf.org/id/draft-ford-cfrg-cosi-00.html
+ * It differs from it that we only implement n-of-n signatures,
+ * simplifying some of the protocol steps.
+ */
+
+/*
+ * Adds two points on a curve (e.g., public keys or commitments).
+ */
+
+void ed25519_add_points(unsigned char *point_AB, const unsigned char *point_A, const unsigned char *point_B) {
+    ge_p1p1 AB_p1p1;
+    ge_p3 AB;
+    /* point A related */
+    ge_p3 point_A_unpacked;
+    ge_cached pA;
+    /* point B related */
+    ge_p3 point_B_unpacked;
+    ge_cached pB;
+    
+    /* unpack point A into pA */
+    ge_frombytes_negate_vartime(&point_A_unpacked, point_A);
+    fe_neg(point_A_unpacked.X, point_A_unpacked.X); /* undo negate */
+    fe_neg(point_A_unpacked.T, point_A_unpacked.T); /* undo negate */
+    ge_p3_to_cached(&pA, &point_A_unpacked);
+
+    /* unpack point B into pB */
+    ge_frombytes_negate_vartime(&point_B_unpacked, point_B);
+    fe_neg(point_B_unpacked.X, point_B_unpacked.X); /* undo negate */
+    fe_neg(point_B_unpacked.T, point_B_unpacked.T); /* undo negate */
+    ge_p3_to_cached(&pB, &point_B_unpacked);
+    
+    /* AB = A + B */
+    ge_add(&AB_p1p1, &A, &B);
+    ge_p1p1_to_p3(&AB, &AB_p1p1);
+
+    /* pack point */
+    ge_p3_tobytes(point_AB, &AB);
+}
+
+/*
+ * Adds two scalar values (e.g., partial signatures).
+ */
+
+void ed25519_add_scalars(unsigned char *scalar_AB, const unsigned char *scalar_A, const unsigned char *scalar_B) {
+    const unsigned char SC_1[32] = {1}; /* scalar with value 1 */
+    sc_muladd(scalar_AB, SC_1, scalar_A, scalar_B);
+}
+
+/*
+ * Picks a random scalar r and computes its commitment R = [r]B.
+ */
+
+int ed25519_create_commitment(unsigned char *secret_r, unsigned char *commitment_R, const unsigned char *randomness) {
+    ge_p3 R;
+
+    // Decompress the 32 byte cryptographically secure random data to 64 byte.
+    sha512(ramdomness, 32, secret);
+    sc_reduce(secret);
+
+    // Abort if secret equals 0 mod l or 1 mod l.
+    if (!sc_valid_reduction(secret)) {
+        return 0;
+    }
+
+    // Compute the point [secret]B. Let the string R be the encoding of this point.
+    ge_scalarmult_base(&R, secret);
+    ge_p3_tobytes(commitment, &R);
+
+    return 1;
+}
--- src/native/ed25519/sign.c
+++ src/native/ed25519/sign.c
@@ -3,11 +3,40 @@
 #include "ge.h"
 #include "sc.h"
 
+/*
+ * Computes a challenge c = H(R || A || M), where R is a commitment, A a public key and M a message.
+ * Then it partially signs M by computing r + c*private_key, where r is the secret corresponding to R
+ * and private_key the private key.
+ *
+ * In single-sig mode, r and R were constructed from the private key.
+ * In multi-sig mode, R is the aggregated commitment, r the individual secret, and A the aggregated public key.
+ *
+ * - partial_signature contains the result of the signing.
+ * - message corresponds to the message M to be signed.
+ * - message_len corresponds to the length of the message.
+ * - commitment_R corresponds to R.
+ * - secret_r corresponds to the secret r.
+ * - public_key corresponds to the private key.
+ * - private_key corresponds to the private key.
+ */
+void ed25519_partial_sign(unsigned char *partial_signature, const unsigned char *message, size_t message_len, const unsigned char* commitment_R, const unsigned char *secret_r, const unsigned char *public_key, const unsigned char *private_key) {
+    sha512_context hash;
+    unsigned char hram[64];
+
+    // c = H(R || A || M)
+    sha512_init(&hash);
+    sha512_update(&hash, commitment_R, 32);
+    sha512_update(&hash, public_key, 32);
+    sha512_update(&hash, message, message_len);
+    sha512_final(&hash, hram);
+
+    sc_reduce(hram);
+    // r + c*private_key mod l
+    sc_muladd(partial_signature, hram, private_key, secret_r);
+}
 
 void ed25519_sign(unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key, const unsigned char *private_key) {
-    sha512_context hash;
     unsigned char az[64];
-    unsigned char hram[64];
     unsigned char r[64];
     ge_p3 R;
 
@@ -26,12 +55,5 @@ void ed25519_sign(unsigned char *signature, const unsigned char *message, size_t
     ge_scalarmult_base(&R, r);
     ge_p3_tobytes(signature, &R);
 
-    sha512_init(&hash);
-    sha512_update(&hash, signature, 32);
-    sha512_update(&hash, public_key, 32);
-    sha512_update(&hash, message, message_len);
-    sha512_final(&hash, hram);
-
-    sc_reduce(hram);
-    sc_muladd(signature + 32, hram, az, r);
+    ed25519_partial_sign(signature + 32, message, message_len, signature, &r, public_key, az);
 }
