0fe6a223ddf8e0ad11c368bf156cace93b2b8545
sisou <hello@soerenschwert.de>
2017-12-17 14:30:09 +0100

    Make statistics output opt-in, document in README

--- clients/nodejs/index.js
+++ clients/nodejs/index.js
@@ -2,13 +2,14 @@ const Nimiq = require('../../dist/node.js');
 const argv = require('minimist')(process.argv.slice(2));
 
 if (!argv.host || !argv.port || !argv.key || !argv.cert) {
-    console.log('Usage: node index.js --host=<hostname> --port=<port> --key=<ssl-key> --cert=<ssl-cert> [--wallet-seed=<wallet-seed>] [--miner[=<thread-num>[:<throttle-after>[:<throttle-wait>]]]] [--passive] [--log=LEVEL] [--log-tag=TAG[:LEVEL]]');
+    console.log('Usage: node index.js --host=<hostname> --port=<port> --key=<ssl-key> --cert=<ssl-cert> [--wallet-seed=<wallet-seed>] [--miner[=<thread-num>[:<throttle-after>[:<throttle-wait>]]]] [--statistics[=<interval>]] [--passive] [--log=LEVEL] [--log-tag=TAG[:LEVEL]]');
     process.exit();
 }
 
 const host = argv.host;
 const port = parseInt(argv.port);
 const minerOptions = argv.miner;
+const statisticsOptions = argv.statistics;
 const passive = argv.passive;
 const key = argv.key;
 const cert = argv.cert;
@@ -27,7 +28,7 @@ if (argv['log-tag']) {
     });
 }
 
-console.log(`Nimiq NodeJS Client starting (host=${host}, port=${port}, miner=${!!minerOptions}, passive=${!!passive})`);
+console.log(`Nimiq NodeJS Client starting (host=${host}, port=${port}, miner=${!!minerOptions}, statistics=${!!statisticsOptions}, passive=${!!passive})`);
 
 // XXX Configure Core.
 // TODO Create config/options object and pass to Core.get()/init().
@@ -92,21 +93,23 @@ const $ = {};
         Nimiq.Log.i(TAG, `Block mined: ${block.header}`);
     });
 
-    // Output regular statistics
-    const hashrates      = [];
-    const outputInterval = 10; // Seconds
+    if (statisticsOptions) {
+        // Output regular statistics
+        const hashrates      = [];
+        const outputInterval = typeof statisticsOptions === 'number' ? statisticsOptions : 10; // seconds
 
-    $.miner.on('hashrate-changed', async (hashrate) => {
-        hashrates.push(hashrate);
+        $.miner.on('hashrate-changed', async (hashrate) => {
+            hashrates.push(hashrate);
 
-        if(hashrates.length >= outputInterval) {
-            let sum = hashrates.reduce((acc, val) => acc += val);
-            Nimiq.Log.i(TAG, `Hashrate: ${(sum / hashrates.length).toFixed(Math.log10(hashrates.length)).padStart(7)} H/s`
-                        + ` - Balance: ${Nimiq.Policy.satoshisToCoins((await $.accounts.get($.wallet.address)).balance)} NIM`
-                        + ` - Mempool: ${$.mempool.getTransactions().length} tx`);
-            hashrates.length = 0;
-        }
-    });
+            if(hashrates.length >= outputInterval) {
+                let sum = hashrates.reduce((acc, val) => acc += val);
+                Nimiq.Log.i(TAG, `Hashrate: ${(sum / hashrates.length).toFixed(Math.log10(hashrates.length)).padStart(7)} H/s`
+                            + ` - Balance: ${Nimiq.Policy.satoshisToCoins((await $.accounts.get($.wallet.address)).balance)} NIM`
+                            + ` - Mempool: ${$.mempool.getTransactions().length} tx`);
+                hashrates.length = 0;
+            }
+        });
+    }
 })().catch(e => {
     console.error(e);
     process.exit(1);
