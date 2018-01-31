96c224bf71cda6cd631f1ea3f4f194b441c78596
danimoh <github.danimoh@neverbox.com>
2018-01-31 11:51:39 -0600

    dev-ui: Account Info UI

---
+++ clients/dev-ui/Utils.js
@@ -0,0 +1,32 @@
+class Utils {
+    static getAccount($, address) {
+        if ($.clientType !== DevUI.CLIENT_NANO) {
+            return $.accounts.get(address);
+        } else {
+            return $.consensus.getAccount(address); // TODO can fail
+        }
+    }
+
+    static broadcastTransaction($, tx) {
+        if ($.clientType !== DevUI.CLIENT_NANO) {
+            $.mempool.pushTransaction(tx);
+        } else {
+            $.consensus.relayTransaction(tx); // TODO can fail
+        }
+    }
+
+    static humanBytes(bytes) {
+        var i = 0;
+        var units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        while (bytes > 1024) {
+            bytes /= 1024;
+            i++;
+        }
+        return (Number.isInteger(bytes) ? bytes : bytes.toFixed(2)) + ' ' + units[i];
+    }
+
+    static toFixedPrecision(value, precision) {
+        precision = precision === undefined? Math.log10(Nimiq.Policy.SATOSHIS_PER_COIN) : precision;
+        return value.toFixed(precision);
+    }
+}
