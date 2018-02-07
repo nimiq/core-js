1524e3764998acadb2379e988b1f5fed64bfd7af
Jeffrey Esquivel S <jeff.esquivel@gmail.com>
2018-02-06 18:12:16 -0600

    First version of MockNetwork and initial test

--- src/main/generic/consensus/Consensus.js
+++ src/main/generic/consensus/Consensus.js
@@ -74,6 +74,78 @@ class Consensus {
 
         return new NanoConsensus(blockchain, mempool, network);
     }
+
+    /**
+     * @param {NetworkConfig} [netconfig]
+     * @return {Promise.<FullConsensus>}
+     */
+    static async volatileFull(netconfig = NetworkConfig.getDefault()) {
+        await Crypto.prepareSyncCryptoWorker();
+
+        netconfig.services = new Services(Services.FULL, Services.FULL);
+        await netconfig.initVolatile();
+
+        /** @type {Time} */
+        const time = new Time();
+        /** @type {Accounts} */
+        const accounts = await Accounts.createVolatile();
+        /** @type {TransactionStore} */
+        const transactionStore = await TransactionStore.createVolatile();
+        /** @type {FullChain} */
+        const blockchain = await FullChain.createVolatile(accounts, time, transactionStore);
+        /** @type {Mempool} */
+        const mempool = new Mempool(blockchain, accounts);
+        /** @type {Network} */
+        const network = new Network(blockchain, netconfig, time);
+
+        return new FullConsensus(blockchain, mempool, network);
+    }
+
+        /**
+     * @param {NetworkConfig} [netconfig]
+     * @return {Promise.<LightConsensus>}
+     */
+    static async volatileLight(netconfig = NetworkConfig.getDefault()) {
+        await Crypto.prepareSyncCryptoWorker();
+
+        netconfig.services = new Services(Services.LIGHT, Services.LIGHT | Services.FULL);
+        await netconfig.initVolatile();
+
+        /** @type {Time} */
+        const time = new Time();
+        /** @type {Accounts} */
+        const accounts = await Accounts.createVolatile();
+        /** @type {LightChain} */
+        const blockchain = await LightChain.createVolatile(accounts, time);
+        /** @type {Mempool} */
+        const mempool = new Mempool(blockchain, accounts);
+        /** @type {Network} */
+        const network = new Network(blockchain, netconfig, time);
+
+        return new LightConsensus(blockchain, mempool, network);
+    }
+
+    /**
+     * @param {NetworkConfig} [netconfig]
+     * @return {Promise.<NanoConsensus>}
+     */
+    static async volatileNano(netconfig = NetworkConfig.getDefault()) {
+        await Crypto.prepareSyncCryptoWorker();
+
+        netconfig.services = new Services(Services.NANO, Services.NANO | Services.LIGHT | Services.FULL);
+        await netconfig.initVolatile();
+
+        /** @type {Time} */
+        const time = new Time();
+        /** @type {NanoChain} */
+        const blockchain = await new NanoChain(time);
+        /** @type {NanoMempool} */
+        const mempool = new NanoMempool(blockchain);
+        /** @type {Network} */
+        const network = new Network(blockchain, netconfig, time);
+
+        return new NanoConsensus(blockchain, mempool, network);
+    }
 }
 
 Class.register(Consensus);
