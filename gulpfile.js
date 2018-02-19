ecdd89c6d3214825c66a172fb306aba78acbffb0
Stefan Mattern <stefan.v.mattern@web.de>
2018-02-19 11:50:03 -0600

    Network state separation: SignalProcessor.js extracted

--- gulpfile.js
+++ gulpfile.js
@@ -129,6 +129,7 @@ const sources = {
         './src/main/generic/network/address/PeerAddress.js',
         './src/main/generic/network/address/PeerAddressState.js',
         './src/main/generic/network/address/PeerAddresses.js',
+        './src/main/generic/network/connection/SignalProcessor.js',
         './src/main/generic/network/connection/ConnectionPool.js',
         './src/main/generic/network/connection/PeerConnection.js',
         './src/main/generic/network/message/Message.js',
