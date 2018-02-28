b20ab0ffcf31f0d52924c8c9748368812d2d85d5
Stefan Mattern <stefan.v.mattern@web.de>
2018-02-28 08:53:08 -0600

    TwoNodes spec fix

--- src/test/specs/generic/network/TwoNodes.spec.js
+++ src/test/specs/generic/network/TwoNodes.spec.js
@@ -47,7 +47,7 @@ describe('TwoNodes', () => {
             const consensus1 = await Consensus.volatileFull(netconfig);
             consensus1.on('established', checkEstablished);
 
-            PeerAddressBook.SEED_PEERS = [WsPeerAddress.seed('node1.test', 9000)];
+            PeerAddressBook.SEED_PEERS = [WsPeerAddress.seed('node1.test', 9000, netconfig.publicKey.toHex())];
 
             const netconfig2 = new RtcNetworkConfig();
             const consensus2 = await Consensus.volatileFull(netconfig2);
