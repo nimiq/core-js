1873f962d6a9dea41c5830f27351a33de53692db
Jeffrey Esquivel S <jeff.esquivel@gmail.com>
2018-03-30 14:49:11 -0600

    Test that RTCPeerConnections are properly closed

--- src/test/specs/generic/network/PeerLeft.spec.js
+++ src/test/specs/generic/network/PeerLeft.spec.js
@@ -26,10 +26,46 @@ describe('PeerLeft', () => {
             const consensus2 = await Consensus.volatileLight(netconfig2);
 
             consensus2.on('established', () => {
-                setTimeout(() => consensus2.network.disconnect("test"), 0);
+                setTimeout(() => consensus2.network.disconnect('test'), 0);
             });
 
             consensus2.network.connect();
+        })().catch(done.fail);
+    });
+
+    it('should properly close RTCPeerConnections', (done) => {
+        let consensus3, establishedCount = 0;
+
+        function checkClosed() {
+            expect(MockNetwork._peerConnectionCounter).toBe(0);
+            done();
+        }
+
+        async function checkEstablished() {
+            establishedCount++;
+            if (establishedCount === 2) {
+                const netconfig3 = new RtcNetworkConfig();
+                consensus3 = await Consensus.volatileLight(netconfig3);
+                consensus3.network.connect();
+                consensus3.on('established', checkEstablished);
+            } else if (establishedCount === 3) {
+                expect(consensus3._agents.length).toBe(2);
+                consensus3.network.disconnect();
+            }
+        }
+
+        (async () => {
+            const netconfig = Dummy.NETCONFIG;
+            const consensus1 = await Consensus.volatileFull(netconfig);
+            consensus1.on('established', checkEstablished);
+            consensus1.network.connect();
+
+            const netconfig2 = new RtcNetworkConfig();
+            const consensus2 = await Consensus.volatileFull(netconfig2);
+            consensus2.network.on('peer-left', checkClosed);
+            consensus2.network.connect();
+
+            consensus2.on('established', checkEstablished);
         })().catch(done.fail);
     });
 });
