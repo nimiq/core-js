b6797c61af14620181ee7b80b818e9ef4caf3bab
sisou <hello@soerenschwert.de>
2018-04-07 18:38:13 -0600

    Add reduced offline gulp build target

---
+++ src/main/generic/consensus/GenesisConfigOffline.js
@@ -0,0 +1,53 @@
+class GenesisConfig {
+    static main() {
+        GenesisConfig.init(GenesisConfig.CONFIGS['main']);
+    }
+
+    static test() {
+        GenesisConfig.init(GenesisConfig.CONFIGS['test']);
+    }
+
+    static dev() {
+        GenesisConfig.init(GenesisConfig.CONFIGS['dev']);
+    }
+
+    static bounty() {
+        GenesisConfig.init(GenesisConfig.CONFIGS['bounty']);
+    }
+
+    /**
+     * @param {{NETWORK_ID:number}} config
+     */
+    static init(config) {
+        if (GenesisConfig._config) throw new Error('GenesisConfig already initialized');
+        if (!config.NETWORK_ID) throw new Error('Config is missing network id');
+
+        GenesisConfig._config = config;
+    }
+
+    /**
+     * @type {number}
+     */
+    static get NETWORK_ID() {
+        if (!GenesisConfig._config) throw new Error('GenesisConfig not initialized');
+        return GenesisConfig._config.NETWORK_ID;
+    }
+}
+Class.register(GenesisConfig);
+
+GenesisConfig.CONFIGS = {
+    // TODO 'main': { }
+    'test': {
+        NETWORK_ID: 1
+    },
+
+    'dev': {
+        NETWORK_ID: 2
+    },
+
+    'bounty': {
+        NETWORK_ID: 3
+    }
+};
+// TODO
+GenesisConfig.CONFIGS['main'] = GenesisConfig.CONFIGS['test'];
