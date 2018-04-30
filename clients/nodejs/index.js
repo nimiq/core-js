c40a0937a7f47ad36e456c89235a7f4a44d631e4
sisou <hello@soerenschwert.de>
2018-05-01 01:15:01 +0200

    Fix --statistics when mining with NanoPoolMiner

--- clients/nodejs/index.js
+++ clients/nodejs/index.js
@@ -239,10 +239,10 @@ const $ = {};
             hashrates.push(hashrate);
 
             if (hashrates.length >= outputInterval) {
-                const account = await $.accounts.get($.wallet.address);
+                const account = !isNano ? await $.accounts.get($.wallet.address) : await $.consensus.getAccount($.wallet.address);
                 const sum = hashrates.reduce((acc, val) => acc + val, 0);
                 Nimiq.Log.i(TAG, `Hashrate: ${(sum / hashrates.length).toFixed(2).padStart(7)} H/s`
-                    + (!isNano ? ` - Balance: ${Nimiq.Policy.satoshisToCoins(account.balance)} NIM` : '')
+                    + ` - Balance: ${Nimiq.Policy.satoshisToCoins(account.balance)} NIM`
                     + ` - Mempool: ${$.mempool.getTransactions().length} tx`);
                 hashrates.length = 0;
             }
