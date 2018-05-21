6f8569768f31f5e23c02fd75cb7839d7df0ba967
Jeffrey Esquivel S <jeff.esquivel@gmail.com>
2018-05-21 13:44:38 -0600

    Support different WebSocket engine options

--- src/main/platform/nodejs/index.prefix.js
+++ src/main/platform/nodejs/index.prefix.js
@@ -5,9 +5,11 @@ const JDB = require('@nimiq/jungle-db');
 const fs = require('fs');
 const dns = require('dns');
 const https = require('https');
-const WebSocket = require('uws');
 const NodeNative = require('bindings')('nimiq_node.node');
 const chalk = require('chalk');
+
+// Allow the user to specify the WebSocket engine through an environment variable. Default to ws
+const WebSocket = require(process.env.NIMIQ_WS_ENGINE || 'ws');
 
 global.Class = {
     scope: module.exports,
