b9c50767a131ee575e8ef650a8574c69778f4e32
Jeffrey Esquivel S <jeff@nimiq.com>
2018-12-25 02:53:18 +0100

    Add basic SubscribeMessage test

--- src/test/specs/generic/network/message/SubscribeMessage.spec.js
+++ src/test/specs/generic/network/message/SubscribeMessage.spec.js
@@ -1,25 +1,19 @@
-describe('GetDataMessage', () => {
-    const vector1 = new InvVector(InvVector.Type.BLOCK, Hash.fromBase64(Dummy.hash1));
-    const vector2 = new InvVector(InvVector.Type.BLOCK, Hash.fromBase64(Dummy.hash2));
-    const vector3 = new InvVector(InvVector.Type.TRANSACTION, Hash.fromBase64(Dummy.hash3));
-    const vectors = [vector1, vector2, vector3];
+describe('SubscribeMessage', () => {
+    const address1 = Address.fromBase64(Dummy.address1);
+    const address2 = Address.fromBase64(Dummy.address2);
+    const address3 = Address.fromBase64(Dummy.address3);
+    const address4 = Address.fromBase64(Dummy.address4);
+    const addresses = [address1, address2, address3, address4];
+    const subscription = Subscription.fromAddresses(addresses);
 
     it('is serializable and unserializable', () => {
-        const msg1 = new GetDataMessage(vectors);
-        const msg2 = GetDataMessage.unserialize(msg1.serialize());
+        const msg1 = new SubscribeMessage(subscription);
+        const msg2 = SubscribeMessage.unserialize(msg1.serialize());
 
-        expect(msg2.vectors.length).toEqual(vectors.length);
-        expect(msg2.vectors[0].equals(vector1)).toBe(true);
-        expect(msg2.vectors[1].equals(vector2)).toBe(true);
-        expect(msg2.vectors[2].equals(vector3)).toBe(true);
-    });
-	
-    it('must have well defined vectors', () => {
-        expect( () => {
-            const test1 = new GetDataMessage(undefined);
-        }).toThrow('Malformed vectors');
-        expect( () => {
-            const test1 = new GetDataMessage([undefined]);
-        }).toThrow('Malformed vectors');
+        expect(msg2.subscription.addresses.length).toEqual(addresses.length);
+        expect(msg2.subscription.addresses[0].equals(address1)).toBe(true);
+        expect(msg2.subscription.addresses[1].equals(address2)).toBe(true);
+        expect(msg2.subscription.addresses[2].equals(address3)).toBe(true);
+        expect(msg2.subscription.addresses[3].equals(address4)).toBe(true);
     });
 });
