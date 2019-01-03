d64b72fbba59c8691482fc8fe2d6a279c5d0997d
sisou <hello@soerenschwert.de>
2019-01-03 14:16:13 +0100

    Add Nimiq.load() method

--- src/main/platform/browser/module.suffix.js
+++ src/main/platform/browser/module.suffix.js
@@ -1,1 +1,10 @@
+/**
+ * @param {string} [path]
+ * @returns {Promise<void>}
+ */
+Nimiq.load = function(path) {
+    if (path) Nimiq._path = path;
+    return Nimiq.WasmHelper.doImportBrowser();
+}
+
 export default Nimiq;
