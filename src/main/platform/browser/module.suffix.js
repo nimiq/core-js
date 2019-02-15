b2130597cff440caf3b045689b5520aa3608cfac
sisou <hello@soerenschwert.de>
2019-02-15 11:13:42 -0600

    For esm module, add Nimiq to the global scope to enable WASM loading

--- src/main/platform/browser/module.suffix.js
+++ src/main/platform/browser/module.suffix.js
@@ -3,6 +3,7 @@
  * @returns {Promise<void>}
  */
 Nimiq.load = function(path) {
+    if (typeof window !== 'undefined') window.Nimiq = Nimiq;
     if (path) Nimiq._path = path;
     return Nimiq.WasmHelper.doImportBrowser();
 }
