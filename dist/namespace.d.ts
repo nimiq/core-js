e7220fc9e7a635d993351fda1b308e1bae35335c
Don Maxito <maxharz@gmail.com>
2019-02-18 18:41:20 -0600

    fix namespace declaration

--- dist/namespace.d.ts
+++ dist/namespace.d.ts
@@ -1,8 +1,8 @@
-import * as _Nimiq from './types';
-
-declare global {
-    namespace Nimiq {}
-}
+import _Nimiq from './types';
 
 export as namespace Nimiq;
 export = _Nimiq;
+
+declare global {
+    const Nimiq: typeof _Nimiq;
+}
